3d6d73a7a50bed81c37a19dd8ad31e2fa2519e2c
kristenlc <admin@kristen.lc>
2022-12-14 13:43:50 -0600

    Revert "fix dyld: cannot load 'Kernel64Patcher' (load command 0x80000034 is unknown)"
    This reverts commit b6a3206431d3eeaeb6fb98f52a17d9f7634979cc.

--- palera1n.sh
+++ palera1n.sh
@@ -752,10 +752,9 @@ if [ ! -f blobs/"$deviceid"-"$version".der ]; then
     remote_cp root@localhost:/mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kcache.patched work/
     if [ "$tweaks" = "1" ]; then
         if [[ "$version" == *"16"* ]]; then
-            # this only works on mac os catalina or above
-            "$dir"/Kernel64Patcher-ios16 work/kcache.patched work/kcache.patched2 -e -o -u -l -t -h
+            "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e -o -u -l -t -h
         else
-            "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e
+            "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e -l
         fi
     else
         "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -a
