17533caffc6fb7f5ab6d414fa3d16795f6a5f8b3
kristenlc <admin@kristen.lc>
2022-12-26 16:26:42 -0600

    fix palera1n not showing on home screen ios 16, and fix sileo ios 15

--- other/rootfs/jbin/post.sh
+++ other/rootfs/jbin/post.sh
@@ -5,9 +5,27 @@ $binpack/bin/mkdir -p /var/.palera1n
 $binpack/bin/rm -rf /var/.palera1n/loader.app
 $binpack/bin/cp -R /jbin/loader.app /var/.palera1n
 $binpack/usr/bin/uicache -p /var/.palera1n/loader.app
+$binpack/usr/bin/uicache -a
 
-# respring
-$binpack/usr/bin/killall -9 SpringBoard
+if [ -e /etc/rc.d/substitute-launcher ]; then
+    /sbin/mount -uw /private/preboot
+    /sbin/mount -uw /
+    /bin/launchctl bootstrap system /Library/LaunchDaemons
+    /usr/libexec/firmware
+    /usr/bin/ldid -s /usr/bin/apt
+    /usr/bin/ldid -s /etc/rc.d/substitute-launcher
+    /etc/rc.d/substitute-launcher
+    /usr/libexec/firmware
+    /usr/bin/sh /launch_ssh_daemon.sh &
+    # respring
+    /usr/bin/sbreload
 
-echo "[post.sh] done"
-exit
+    echo "[post.sh] done"
+    exit
+else
+    # respring
+    $binpack/usr/bin/killall -9 SpringBoard
+
+    echo "[post.sh] done"
+    exit
+fi
